eb2100452efc341d1859eb752b7eae0278fda8d9
anthonykimani <kimaniantoni@gmail.com>
2024-05-09 12:52:46 +0300

    feat: creates frontend repository

---
+++ frontend/app/home/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from 'react'
+import Navbar from './components/Navbar';
+
+const Home = () => {
+  return (
+    <section className='bg-white text-black font-lexend'>
+        <Navbar />
+    </section>
+  )
+}
+
+export default Home
